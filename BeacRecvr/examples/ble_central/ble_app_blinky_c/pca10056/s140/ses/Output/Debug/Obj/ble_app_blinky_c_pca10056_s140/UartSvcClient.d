fe8ee5b6cb6a2a9d9df8cdf6ec421bf41da57759
Adhil <adhilxavier@gmail.com>
2024-01-19 17:55:15 +0530

    Added Beacon receiver

---
+++ BeacRecvr/examples/ble_central/ble_app_blinky_c/pca10056/s140/ses/Output/Debug/Obj/ble_app_blinky_c_pca10056_s140/UartSvcClient.d
@@ -0,0 +1,2 @@
+Output/Debug/Obj/ble_app_blinky_c_pca10056_s140/UartSvcClient.o: \
+ C:\Users\adhil\OneDrive\Desktop\Train\BeaconTest\Ble_Beacon_Reciever\examples\ble_central\ble_app_blinky_c\UartSvcClient.c
